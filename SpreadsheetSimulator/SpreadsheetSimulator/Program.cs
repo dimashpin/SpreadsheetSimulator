3fa29219d15657f390df7071bba9be845f573816
dimashpin <dimashpin@outlook.com>
2015-10-20 16:24:07 +0300

    Reading from the text file algorithm

--- SpreadsheetSimulator/SpreadsheetSimulator/Program.cs
+++ SpreadsheetSimulator/SpreadsheetSimulator/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SpreadsheetSimulator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //if (!File.Exists("Source.txt"))
+            //{
+            //    throw new Exception("The file path doesn't exist");
+            //}
+
+            StreamReader MyStreamReader = new StreamReader(@"C:\Users\Dimas\Source\Repos\SpreadsheetSimulator\SpreadsheetSimulator\SpreadsheetSimulator\Source.txt");
+
+            string temp = "";
+            int i = 0;
+            while (temp != null)
+            {
+                temp = MyStreamReader.ReadLine();
+                if (temp != null)
+                {
+                    foreach (char s in temp)
+                    {
+                       Console.WriteLine(s);
+                    }
+                }
+                //Console.WriteLine(temp);
+                //Console.WriteLine(i);
+            }
+
         }
     }
 }
