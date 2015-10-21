2ac30de73084e5b88fa8c121c3e81373adbd6009
dimashpin <dimashpin@outlook.com>
2015-10-21 12:04:41 +0300

    Added TableParse class

---
+++ SpreadsheetSimulator/SpreadsheetSimulator/TableParse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace SpreadsheetSimulator
+{
+    class TableParse
+    {
+        string line = "";
+        string cell = null;
+
+        public void tbParse(string path)
+        {
+            StreamReader MyStreamReader = new StreamReader(path);
+            while (line != null)
+            {
+                line = MyStreamReader.ReadLine();
+                if (line != null)
+                {
+
+                    foreach (char ch in line)
+                    {
+                        if (ch != '\t') cell += ch;     
+                        else
+                        {
+                            Console.WriteLine(cell);
+                            cell = null;
+                        }
+                    }
+                    Console.WriteLine(cell);
+                    cell = null;
+                }
+            }
+        }
+    }
+}
