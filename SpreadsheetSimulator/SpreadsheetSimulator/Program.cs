8d2a73f459ebfadb5907563bcff2a17f6d88bedb
dimashpin <dimashpin@outlook.com>
2015-10-21 14:27:22 +0300

    TableParse method improving. TablePrint method added. Cells is now adding into the list. Cells now may also containg text

--- SpreadsheetSimulator/SpreadsheetSimulator/Program.cs
+++ SpreadsheetSimulator/SpreadsheetSimulator/Program.cs
@@ -10,6 +10,10 @@ namespace SpreadsheetSimulator
         {
             TableParse T1 = new TableParse();
             T1.tbParse(@"C:\Users\Dimas\Source\Repos\SpreadsheetSimulator\SpreadsheetSimulator\SpreadsheetSimulator\Source.txt");
+            T1.PrintCell();
+            Console.WriteLine();
+            T1.PrintCell(1, 2);
+
         }
     }
 }
--- SpreadsheetSimulator/SpreadsheetSimulator/TableParse.cs
+++ SpreadsheetSimulator/SpreadsheetSimulator/TableParse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace SpreadsheetSimulator
 {
@@ -7,13 +8,20 @@ namespace SpreadsheetSimulator
     {
         string line = "";
         string cell = null;
+        public int row { get; set; }
+        public int column { get; set; }
+        List<List<object>> grid = new List<List<object>>();
 
+        //parsing through the output
         public void tbParse(string path)
         {
             StreamReader MyStreamReader = new StreamReader(path);
+            row = 0;
+            column = 0;
             while (line != null)
             {
                 line = MyStreamReader.ReadLine();
+                grid.Add(new List<object>());
                 if (line != null)
                 {
 
@@ -22,14 +30,39 @@ namespace SpreadsheetSimulator
                         if (ch != '\t') cell += ch;     
                         else
                         {
-                            Console.WriteLine(cell);
+                            grid[column].Add(cell);
+                            row++;
+                            //Console.WriteLine(cell);
                             cell = null;
                         }
                     }
-                    Console.WriteLine(cell);
+                    grid[column].Add(cell);
+                    row++;
+                    //Console.WriteLine(cell);
                     cell = null;
                 }
+                column++;
             }
+        }
+
+        //printing all cell from the output
+        public void PrintCell()
+        {
+            foreach (List<object> r in grid)
+            {
+                foreach (object c in r)
+                {
+                    Console.Write(c+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //printing one cell from the output
+        public void PrintCell(int row, int column)
+        {
+            Console.WriteLine("The value of cell with the adress row = {0}, column = {1} is {2}",
+                row, column, grid[row][column]);
         }
     }
 }
